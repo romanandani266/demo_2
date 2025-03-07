e5c2e5369417f7c6b685af1cb5d26656ecbfc825
romanandani266 <romanandani266@gmail.com>
2025-03-07 19:16:17 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,27 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import HomePage from './pages/HomePage';
-import CreateEditPage from './pages/CreateEditPage';
-import BlogDetailPage from './pages/BlogDetailPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline, Container } from "@mui/material";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import BlogPage from "./pages/BlogPage";
+import CreateBlogPage from "./pages/CreateBlogPage";
+import EditBlogPage from "./pages/EditBlogPage";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            My Blog Platform
-          </Typography>
-          <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/create">Create Blog</Button>
-        </Toolbar>
-      </AppBar>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreateEditPage />} />
-        <Route path="/edit/:id" element={<CreateEditPage />} />
-        <Route path="/blogs/:id" element={<BlogDetailPage />} />
-      </Routes>
-    </div>
+    <>
+      <CssBaseline />
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blogs/:id" element={<BlogPage />} />
+          <Route path="/create" element={<CreateBlogPage />} />
+          <Route path="/edit/:id" element={<EditBlogPage />} />
+        </Routes>
+      </Container>
+    </>
   );
-};
+}
 
 export default App;
