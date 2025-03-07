581ca5675fce94a57b6d336a06e08c24adef1874
romanandani266 <romanandani266@gmail.com>
2025-03-07 19:41:28 +0530

    Add frontend/src/api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:8080";
 
-export const fetchBlogs = async () => {
+export const getBlogs = async () => {
   try {
     const response = await axios.get(`${API_BASE_URL}/blogs`);
     return response.data;
@@ -12,7 +12,7 @@ export const fetchBlogs = async () => {
   }
 };
 
-export const fetchBlogById = async (id) => {
+export const getBlog = async (id) => {
   try {
     const response = await axios.get(`${API_BASE_URL}/blogs/${id}`);
     return response.data;
