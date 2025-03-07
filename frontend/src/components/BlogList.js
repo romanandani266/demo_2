9f4277dc01ba36b32e9fd38bc3f277d0517aabc6
romanandani266 <romanandani266@gmail.com>
2025-03-07 16:44:49 +0530

    Add frontend/src/components/BlogList.js

--- frontend/src/components/BlogList.js
+++ frontend/src/components/BlogList.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography, CardMedia } from "@mui/material";
 
 const BlogList = ({ blogs }) => {
   return (
     <div>
       {blogs.map((blog) => (
-        <Card key={blog.id} style={{ marginBottom: "16px" }}>
+        <Card key={blog.id} sx={{ marginBottom: 2 }}>
+          {blog.image_url && (
+            <CardMedia
+              component="img"
+              height="140"
+              image={blog.image_url}
+              alt={blog.title}
+            />
+          )}
           <CardContent>
             <Typography variant="h5">{blog.title}</Typography>
-            <Typography variant="body2">{blog.content.substring(0, 100)}...</Typography>
+            <Typography variant="body2">{blog.content_snippet}</Typography>
             <Link to={`/blogs/${blog.id}`}>Read More</Link>
           </CardContent>
         </Card>
