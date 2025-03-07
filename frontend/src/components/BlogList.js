76133925c1644e4744cf4a79d55d6096814fceb7
romanandani266 <romanandani266@gmail.com>
2025-03-07 19:41:24 +0530

    Add frontend/src/components/BlogList.js

--- frontend/src/components/BlogList.js
+++ frontend/src/components/BlogList.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getBlogs } from "../api";
 import { Link } from "react-router-dom";
-import { Card, CardContent, Typography, CardMedia } from "@mui/material";
+import { Card, CardContent, Typography, Grid } from "@mui/material";
 
-const BlogList = ({ blogs }) => {
+const BlogList = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const data = await getBlogs();
+        setBlogs(data);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+    fetchBlogs();
+  }, []);
+
   return (
-    <div>
+    <Grid container spacing={2}>
       {blogs.map((blog) => (
-        <Card key={blog.id} sx={{ marginBottom: 2 }}>
-          {blog.image_url && (
-            <CardMedia
-              component="img"
-              height="140"
-              image={blog.image_url}
-              alt={blog.title}
-            />
-          )}
-          <CardContent>
-            <Typography variant="h5">{blog.title}</Typography>
-            <Typography variant="body2">{blog.content_snippet}</Typography>
-            <Link to={`/blogs/${blog.id}`}>Read More</Link>
-          </CardContent>
-        </Card>
+        <Grid item xs={12} key={blog.id}>
+          <Card>
+            <CardContent>
+              <Typography variant="h5">{blog.title}</Typography>
+              <Typography variant="body2">{blog.content.slice(0, 100)}...</Typography>
+              <Link to={`/blogs/${blog.id}`}>Read More</Link>
+            </CardContent>
+          </Card>
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 };
 
