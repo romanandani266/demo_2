be348ef7ce78990995bc7693e5a57973b3434377
romanandani266 <romanandani266@gmail.com>
2025-03-07 16:13:52 +0530

    Add frontend/src/pages/Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { getAllBlogs } from "../api";
+import BlogList from "../components/BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const data = await getAllBlogs();
+        setBlogs(data);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    };
+    fetchBlogs();
+  }, []);
+
+  return <BlogList blogs={blogs} />;
+};
+
+export default Home;
