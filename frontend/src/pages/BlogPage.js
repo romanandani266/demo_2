5f325c92c35e7638e3a69d7572be9dbb46bc3606
romanandani266 <romanandani266@gmail.com>
2025-03-07 19:16:22 +0530

    Add frontend/src/pages/BlogPage.js

--- frontend/src/pages/BlogPage.js
+++ frontend/src/pages/BlogPage.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchBlogById } from "../api";
+import { Typography, Box } from "@mui/material";
 
-const BlogPage = () => {
+function BlogPage() {
   const { id } = useParams();
   const [blog, setBlog] = useState(null);
 
@@ -12,21 +13,21 @@ const BlogPage = () => {
         const data = await fetchBlogById(id);
         setBlog(data);
       } catch (error) {
-        console.error("Error loading blog:", error);
+        console.error("Failed to load blog:", error);
       }
     };
     loadBlog();
   }, [id]);
 
-  if (!blog) return <div>Loading...</div>;
+  if (!blog) return <Typography>Loading...</Typography>;
 
   return (
-    <div>
-      <h1>{blog.title}</h1>
-      <p>{blog.content}</p>
-      {blog.image_url && <img src={blog.image_url} alt={blog.title} />}
-    </div>
+    <Box>
+      <Typography variant="h3">{blog.title}</Typography>
+      <Typography variant="body1">{blog.content}</Typography>
+      {blog.image_url && <img src={blog.image_url} alt={blog.title} style={{ width: "100%" }} />}
+    </Box>
   );
-};
+}
 
 export default BlogPage;
