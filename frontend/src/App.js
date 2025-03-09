5256545a8f654b53ab01bcb6f939e53569204106
romanandani266 <romanandani266@gmail.com>
2025-03-09 19:35:30 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import BlogList from "./components/BlogList";
-import BlogDetail from "./components/BlogDetail";
-import BlogForm from "./components/BlogForm";
+import Login from "./components/Login";
+import Inventory from "./components/Inventory";
+import Alerts from "./components/Alerts";
+import SalesTrends from "./components/SalesTrends";
 
 const App = () => {
+  const [token, setToken] = useState(null);
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<BlogList />} />
-        <Route path="/blogs/:id" element={<BlogDetail />} />
-        <Route path="/create" element={<BlogForm />} />
+        <Route path="/" element={<h1>Welcome to Retail Inventory Management</h1>} />
+        <Route path="/inventory" element={<Inventory token={token} />} />
+        <Route path="/alerts" element={<Alerts token={token} />} />
+        <Route path="/sales-trends" element={<SalesTrends token={token} />} />
       </Routes>
     </>
   );
