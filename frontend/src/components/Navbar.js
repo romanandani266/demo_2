6335d23a60bc96fa25c9ee5607c9952964c9d8b1
romanandani266 <romanandani266@gmail.com>
2025-03-09 19:35:32 +0530

    Add frontend/src/components/Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { AppBar, Toolbar, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Modern Blog Platform
-        </Typography>
         <Button color="inherit" component={Link} to="/">
           Home
         </Button>
-        <Button color="inherit" component={Link} to="/create">
-          Create Blog
+        <Button color="inherit" component={Link} to="/inventory">
+          Inventory
+        </Button>
+        <Button color="inherit" component={Link} to="/alerts">
+          Alerts
+        </Button>
+        <Button color="inherit" component={Link} to="/sales-trends">
+          Sales Trends
         </Button>
       </Toolbar>
     </AppBar>
