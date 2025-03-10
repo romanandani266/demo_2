b99be0a9ebf73e6da795a018bf819a66807e04e2
romanandani266 <romanandani266@gmail.com>
2025-03-10 19:48:48 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import theme from './theme';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import CreateEditBlog from './pages/CreateEditBlog';
-import BlogDetailPage from './pages/BlogDetailPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CreateEditPage from "./pages/CreateEditPage";
+import BlogDetailPage from "./pages/BlogDetailPage";
 
-function App() {
+const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateEditBlog />} />
-        <Route path="/edit/:id" element={<CreateEditBlog />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreateEditPage />} />
+        <Route path="/edit/:id" element={<CreateEditPage />} />
         <Route path="/blogs/:id" element={<BlogDetailPage />} />
       </Routes>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
