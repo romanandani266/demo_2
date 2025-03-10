83c4960b3bfe84cb62f0c403fe36a4a1a52cbdc0
romanandani266 <romanandani266@gmail.com>
2025-03-10 19:00:20 +0530

    Add frontend/src/pages/Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,33 @@
-import React from "react";
-import BlogList from "../components/BlogList";
+import React, { useEffect, useState } from 'react';
+import { getAllBlogs } from '../api';
+import BlogCard from '../components/BlogCard';
+import { Grid, Container } from '@mui/material';
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const data = await getAllBlogs();
+        setBlogs(data);
+      } catch (error) {
+        console.error('Error fetching blogs:', error);
+      }
+    };
+    fetchBlogs();
+  }, []);
+
   return (
-    <div>
-      <h1>Welcome to the Modern Blog Platform</h1>
-      <BlogList />
-    </div>
+    <Container>
+      <Grid container spacing={3}>
+        {blogs.map((blog) => (
+          <Grid item xs={12} sm={6} md={4} key={blog.id}>
+            <BlogCard blog={blog} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
