84ca1cb06443d7eb1152fbe11e0063a9ab83195e
romanandani266 <romanandani266@gmail.com>
2025-03-10 11:44:35 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import CreateBlog from "./pages/CreateBlog";
 import Navbar from "./components/Navbar";
-import Inventory from "./components/Inventory";
-import Alerts from "./components/Alerts";
-import SalesTrends from "./components/SalesTrends";
-import Login from "./components/Login";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<h1>Welcome to Retail Inventory Management</h1>} />
-        <Route path="/inventory" element={<Inventory />} />
-        <Route path="/alerts" element={<Alerts />} />
-        <Route path="/sales-trends" element={<SalesTrends />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/create" element={<CreateBlog />} />
       </Routes>
     </div>
   );
