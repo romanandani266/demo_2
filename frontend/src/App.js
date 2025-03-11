0f680fec236b5d1f452542ae3c84c9ed4bb3deeb
romanandani266 <romanandani266@gmail.com>
2025-03-11 15:58:52 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
-import HomePage from "./pages/HomePage";
-import CreateEditPage from "./pages/CreateEditPage";
-import BlogDetailPage from "./pages/BlogDetailPage";
+import { AppBar, Toolbar, Button, Typography } from "@mui/material";
+import Inventory from "./components/Inventory";
+import Alerts from "./components/Alerts";
+import SalesTrends from "./components/SalesTrends";
+import Products from "./components/Products";
+import Locations from "./components/Locations";
+import Login from "./components/Login";
 
 const App = () => {
   return (
@@ -11,22 +14,36 @@ const App = () => {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" style={{ flexGrow: 1 }}>
-            My Blog App
+            Retail Inventory Management
           </Typography>
-          <Button color="inherit" component={Link} to="/">
-            Home
+          <Button color="inherit" component={Link} to="/inventory">
+            Inventory
           </Button>
-          <Button color="inherit" component={Link} to="/create">
-            Create Blog
+          <Button color="inherit" component={Link} to="/alerts">
+            Alerts
+          </Button>
+          <Button color="inherit" component={Link} to="/sales-trends">
+            Sales Trends
+          </Button>
+          <Button color="inherit" component={Link} to="/products">
+            Products
+          </Button>
+          <Button color="inherit" component={Link} to="/locations">
+            Locations
+          </Button>
+          <Button color="inherit" component={Link} to="/login">
+            Login
           </Button>
         </Toolbar>
       </AppBar>
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreateEditPage />} />
-        <Route path="/edit/:id" element={<CreateEditPage />} />
-        <Route path="/blogs/:id" element={<BlogDetailPage />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/alerts" element={<Alerts />} />
+        <Route path="/sales-trends" element={<SalesTrends />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
